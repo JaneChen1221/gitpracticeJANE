aa021baeba509fa4198c2c45cf0480cfdc9f67ae
Minyu Chen <daisychen@Minyus-MacBook-Pro.local>
2020-02-26 22:19:00 -0800

    added last name

--- practiceJANE.py
+++ practiceJANE.py
@@ -1,5 +1,5 @@
-# Partner 1 Name: Jane
-# Partner 2 Name: Daisy
+# Partner 1 Name: Jane Chen
+# Partner 2 Name: Daisy Chen
 ############################
 def getNRandom(n):
 	'''takes in an integer and returns a list of n random integers between 1 and 10, inclusive'''
